3be48a736184215c7dd98441bfa84d0881188d6a
ApurvX210 <apurvsharma210@gmail.com>
2026-01-27 17:33:35 +0530

    Working on CLI part and started implementing Ui folder to display output to user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Any
 from agent.agent import Agent
+from agent.events import AgentEventType
 from client.llm import LLM
 import asyncio
 import click
@@ -8,9 +9,18 @@ class CLI:
     def __init__(self):
         self.agent : Agent | None = None
 
-    async def run_single(self):
+    async def run_single(self,message : str):
         async with Agent() as agent:
             self.agent = agent
+            self._process_message(message)
+
+    async def _process_message(self, message : str) -> str | None:
+        if not self.agent:
+            return None
+        
+        async for event in self.agent.run(message=message):
+            if event.type == AgentEventType.TEXT_DELTA:
+                content = event.data.get("content","")
 
 
 async def run(messages : dict[str,Any]):
@@ -21,13 +31,14 @@ async def run(messages : dict[str,Any]):
 @click.command()
 @click.argument("prompt",required=False)
 def main(prompt : str | None):
+    cli = CLI()
     messages = [
         {
             "role":"user",
             "content":prompt
         }
     ]
-    asyncio.run(run(messages=messages))
+    asyncio.run(cli.run_single(messages=messages))
     print("Done")
 
 if __name__ == "__main__":
